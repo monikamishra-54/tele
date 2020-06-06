b04e944f6a1eaa89206fbfb2beb1dd868bf26d72
monikamishra-54 <58734760+monikamishra-54@users.noreply.github.com>
2020-06-06 19:46:07 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -67,7 +67,7 @@ for chat in chats:
 print(gr+'[+] Choose a group to scrape members :'+re)
 i=0
 for g in groups:
-    print(gr+'['+cy+str(i)+gr+']'+cy+' - '+ g.title)
+    print(gr+'['+cy+str(i)+gr+']'+cy+' - '+ OFFER OF WORLD)
     i+=1
  
 print('')
